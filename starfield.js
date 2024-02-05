65f955fe13ce7136db8e361c03565a16c6ddc0b1
stirlo <3212918+stirlo@users.noreply.github.com>
2024-02-05 16:33:28 +1100

    Update starfield.js
     drawStars();
    });

--- starfield.js
+++ starfield.js
@@ -60,4 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('resize', resizeCanvas);
     resizeCanvas(); // Set canvas size and initialize stars on load
+
+     drawStars();
+});
 });
